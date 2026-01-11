9ce344e3a0a86d97469335847971fde724e2158c
NereonNeo <yoqubman21@gmail.com>
2026-01-11 15:27:13 +0500

    feat: docker done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierEsLint from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierEsLint,
+  
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,6 +16,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "commitlint.config.ts"
   ]),
 ]);
 
